27ebf87ceb9381c766c378fcbcfb2fba1cac8b73
sihina3436 <nimnadasihina@gmail.com>
2026-01-25 22:21:11 +0530

    update index.ts file

--- backend/index.ts
+++ backend/index.ts
@@ -1,27 +1,35 @@
-import express, {Application, Request, Response} from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db';
-import userRouter from './router/user.route';
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import http from "http";
+import connectDB from "./src/config/db";
+
+import { initSocket } from "./src/utils/socket";
+import "./src/utils/todoReminder";
+
+import userRouter from "./src/router/user.route";
+import todoRouter from "./src/router/todo.route";
+import categoryRouter from "./src/router/category.route";
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
-
-
-
-
 app.use(express.json());
 
 connectDB();
 
-app.get('/',(req: Request, res: Response) =>{
-    res.status(200).send('Hello, World!');
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello, World!");
 });
 
-app.use('/api/users', userRouter);
+app.use("/api/categories", categoryRouter);
+app.use("/api/users", userRouter);
+app.use("/api/todos", todoRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+const server = http.createServer(app);
+initSocket(server);
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
