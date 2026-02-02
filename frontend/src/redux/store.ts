4cf9cb848faa5690110fc83146a573a68d856845
sihina3436 <nimnadasihina@gmail.com>
2026-02-02 23:41:24 +0530

    configure Redux store with RTK Query middleware and reducers

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,10 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { emptyReducer } from "./emptyReducer";
+
+import authReducer from "./authentication/authSlice";
+import { authApi } from "./authentication/authApi";
+import { todoApi } from "./todo/todoApi";
+import { categoryApi } from "./category/categoryApi";
 
 export const store = configureStore({
-    reducer:{ empty: emptyReducer }
+  reducer: {
+    /* normal slices */
+    auth: authReducer,
+
+    /* RTK Query */
+    [authApi.reducerPath]: authApi.reducer,
+    [todoApi.reducerPath]: todoApi.reducer,
+    [categoryApi.reducerPath]: categoryApi.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      todoApi.middleware,
+      categoryApi.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
