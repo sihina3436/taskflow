ee482648b0a98c14bc871c07f93c937d303292df
sihina3436 <nimnadasihina@gmail.com>
2026-02-12 23:12:01 +0530

    Update index.ts file for Impliment the Socket Programming

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import http from "http";
+import cors from "cors";
 import connectDB from "./src/config/db";
 
 import { initSocket } from "./src/utils/socket";
-import "./src/utils/todoReminder";
+import { startReminder } from "./src/utils/todoReminder";
 
 import userRouter from "./src/router/user.route";
 import todoRouter from "./src/router/todo.route";
@@ -15,21 +16,33 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 connectDB();
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Hello, World!");
+  res.status(200).send("Server running...");
 });
 
 app.use("/api/categories", categoryRouter);
-app.use("/api/users", userRouter);
+app.use("/api/auth", userRouter);
 app.use("/api/todos", todoRouter);
 
 const server = http.createServer(app);
-initSocket(server);
+
+// ✅ Initialize socket first
+const io = initSocket(server);
+
+// ✅ Start reminder AFTER socket is ready
+startReminder(io);
 
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`🚀 Server running on http://localhost:${port}`);
 });
